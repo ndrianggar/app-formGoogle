78ad71adf5d5c0bfa87ca526cf060843ac47b96a
ndrianggar <ndrianggar@gmail.com>
2023-05-13 16:27:21 +0700

    First Commit

---
+++ pages/auth.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+       <h1>AUTH</h1>
+      </v-col>
+    </v-row>
+  </template>
+  <script>
+    export default{
+        layout:'auth', // merouting layout pada foldernya
+        head:{
+            title:'AUTH'
+        }
+    }
+</script>
